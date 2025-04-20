224b35c4c8650e3d30c3ba2e46974cf270eefd9a
Billy Cas <billy25caslin@gmail.com>
2025-04-20 15:05:32 +1000

    remove dum ah temp code runner

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-scaler = MinMaxScaler()
-# player_stats.loc[:, selected_columns] = scaler.fit_transform(player_stats[selected_columns])
-
-# print(player_stats.head())
-# print(player_stats.describe())
-# # print(player_stats.isnull().sum())
-
-# sfs.fit(player_stats[selected_columns], player_stats["Next_Rating"])
-# predictors = list(selected_columns[sfs.get_support()])

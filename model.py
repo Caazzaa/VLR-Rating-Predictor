98d5d5e39fb41da5c4aae210a763bcff3ae4316e
Billy Cas <billy25caslin@gmail.com>
2025-04-23 12:20:47 +1000

    setup predictions for future ratings

--- model.py
+++ model.py
@@ -10,9 +10,6 @@ from sklearn.metrics import mean_squared_error
 
 player_stats = pd.read_csv("player_stats_cleaned.csv", index_col=False)
 
-player_stats_full = player_stats.copy()
-player_stats = player_stats.dropna(subset=["Next_Rating"])
-
 rr = Ridge(alpha=1)
 
 split = TimeSeriesSplit(n_splits=3)
@@ -22,12 +19,11 @@ sfs = SequentialFeatureSelector(rr, n_features_to_select=16, direction="forward"
 removed_columns = ["Player ID", "Player", "Team", "Next_Rating", "Event Title", "Event Number"]
 selected_columns = player_stats.columns[~player_stats.columns.isin(removed_columns)]
 
-# print(player_stats[selected_columns].head())
-
 scaler = MinMaxScaler()
 player_stats.loc[:, selected_columns] = scaler.fit_transform(player_stats[selected_columns])
 
-sfs.fit(player_stats[selected_columns], player_stats["Next_Rating"])
+valid_data = player_stats.dropna(subset=["Next_Rating"])
+sfs.fit(valid_data[selected_columns], valid_data["Next_Rating"])
 predictors = list(selected_columns[sfs.get_support()])
 
 def backtest(data, model, predictors, start=120, step=1):
@@ -38,19 +34,21 @@ def backtest(data, model, predictors, start=120, step=1):
         train = data[data["Event Number"] < current_event]
         test = data[data["Event Number"] == current_event]
 
+        train = train.dropna(subset=["Next_Rating"])
+
         model.fit(train[predictors], train["Next_Rating"])
 
         preds = model.predict(test[predictors])
         preds = pd.Series(preds, index=test.index, name="Predicted Rating")
+
         combined = pd.concat([test["Next_Rating"], preds], axis=1)
         combined.columns = ["Actual Rating", "Predicted Rating"]
-        all_preds.append(combined)
         combined["Player"] = test["Player"].values
         combined["Event Title"] = test["Event Title"].values
+
+        all_preds.append(combined)
     
     return pd.concat(all_preds)
-
-# predictions = backtest(player_stats, rr, predictors, start=5, step=1)
 
 def player_history(df):
     df = df.sort_values("Event Number")
@@ -64,35 +62,26 @@ def player_history(df):
 
     return df
 
-player_stats_full = player_stats.groupby("Player ID", group_keys=False).apply(player_history)
-
 def group_avgs(df):
     return df["Rating"] / df["Rating"].mean()
 
-player_stats_full["rating_season"] = player_stats.groupby("Event Number", group_keys=False).apply(group_avgs)
+
+player_stats = player_stats.groupby("Player ID", group_keys=False).apply(player_history)
+player_stats["rating_season"] = player_stats.groupby("Event Number", group_keys=False).apply(group_avgs)
 
 new_predictors = predictors + ["rating_diff", "rating_corr", "rating_season"]
 
 print(player_stats["Next_Rating"].describe())
 
-new_predictions = backtest(player_stats_full, rr, new_predictors, start=5, step=1)
+new_predictions = backtest(player_stats, rr, new_predictors, start=5, step=1)
 print(new_predictions.isnull().sum())
 print(new_predictions[new_predictions["Event Title"] == "Champions Tour 2025: EMEA Stage 1"])
-print((mean_squared_error(new_predictions["Actual Rating"], new_predictions["Predicted Rating"]))**0.5)
-
-# print(predictions)
-# print((mean_squared_error(predictions["Actual Rating"], predictions["Predicted Rating"]))**0.5)
-
-# new_predictions = backtest(player_stats, rr, new_predictors, start=5, step=1)
-# print(new_predictions)
-# print((mean_squared_error(new_predictions["Actual Rating"], new_predictions["Predicted Rating"]))**0.5)
-
-# print(predictions)
-# print((mean_squared_error(predictions["Actual Rating"], predictions["Predicted Rating"]))**0.5)
+check_error = new_predictions.dropna(subset=["Actual Rating"])
+print((mean_squared_error(check_error["Actual Rating"], check_error["Predicted Rating"]))**0.5)
 
 """
 TODOs:
-- Predict Future Ratings
+- Predict Future Ratings: DONE
 - Explore other models beside ridge regression
 - Engineer knew features to give better context
 - Allow user input to grab the future predictions or predictions for specific player or event
